506c18ad7fc4c889e40a2b0230f3e554097fba31
John Perez <john.perez.quesada@est.una.ac.cr>
2025-05-19 16:58:39 -0600

    Initial commit

---
+++ src/main/java/program2/projectmahn3_0/PrimaryController.java
@@ -0,0 +1,12 @@
+package program2.projectmahn3_0;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
